30f90145df9f603f9757ef8becf5b2ec375732f8
Maksim Verkhoturov <sd32@protonmail.com>
2021-01-15 02:00:41 -0700

    updates server, installs packages

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const routes = require('./routes')
 const PORT = process.env.PORT || 3001
 const app = express()
+const db = require('./models')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -12,6 +13,8 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes)
 
-app.listen(PORT, () => {
-  console.log('Listening on PORT ' + PORT)
+db.sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log('Listening on PORT ' + PORT)
+  })
 })
