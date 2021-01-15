b497758450100e384ac1f7b70118726ecc8b77a1
Maksim Verkhoturov <sd32@protonmail.com>
2021-01-15 03:30:24 -0700

    uses pages, components, globalStyle

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,29 @@
+import React from 'react'
+import { createGlobalStyle } from 'styled-components'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Users from './pages/Users'
+import Nav from './components/Nav'
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    text-align: center;
+  }
+`
+
 function App() {
-  return <h1>Hi!</h1>
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/users" component={Users} />
+        </Switch>
+      </Router>
+    </>
+  )
 }
 
 export default App
