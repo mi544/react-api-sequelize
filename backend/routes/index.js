318e4c2fdbceb2eaa2e74f5815af2c0bb8e7bf30
Maksim Verkhoturov <sd32@protonmail.com>
2021-01-15 02:00:27 -0700

    adds api routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
 const path = require('path')
+const apiRoutes = require('./apiRoutes')
 
-router.get('*', (req, res) => {
+router.use('/api', apiRoutes)
+
+router.use((req, res) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'))
 })
 
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,45 @@
+const router = require('express').Router()
+const db = require('../models')
+const { scs, errs } = require('../msg')
+
+router.get('/:id', async function (req, res) {
+  try {
+    // should be id from the session or token
+    const prId = Number(req.params.id)
+    if (Number.isNaN(prId)) {
+      return res.json(errs.id.format)
+    }
+    const userFound = await db.User.findByPk(prId)
+    if (!userFound) {
+      return res.json(errs.user.notFound)
+    }
+    // save into a token or session instead
+    res.json(scs.user.found(await userFound.toJSON()))
+  } catch (err) {
+    console.log(err)
+    res.json(errs.id.generic)
+  }
+})
+
+router.post('/', async function (req, res) {
+  try {
+    const { name } = req.body
+    if (!name) return res.json(errs.user.notProvided)
+    const newUser = await db.User.create({ name })
+    res.json(scs.user.created(await newUser.toJSON()))
+  } catch (err) {
+    if (
+      err?.name === 'SequelizeUniqueConstraintError' &&
+      err?.errors[0].type === 'unique violation' &&
+      err?.errors[0].path === 'name'
+    ) {
+      return res.json(errs.user.alreadyUsed)
+    }
+    if (err?.name === 'SequelizeValidationError') {
+      return res.json(errs.user.validation(err.errors[0].message))
+    }
+    res.json(errs.user.generic)
+  }
+})
+
+module.exports = router
